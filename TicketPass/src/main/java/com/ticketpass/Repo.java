20a31adae8fe3a986c37bc2903a009d61b54b4ec
oguzkeremyildiz <oguzkeremyildiz@gmail.com>
2026-04-28 12:04:55 +0300

    Repodan çıkar beni

---
+++ TicketPass/src/main/java/com/ticketpass/Repo.java
@@ -0,0 +1,10 @@
+package com.ticketpass;
+
+public class Repo {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("repodan çıkar beni mert");
+        }
+    }
+}
